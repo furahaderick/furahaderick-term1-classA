38d62dd7fd99d8a241673f8eff673d0180c60ecc
furahaderick <yvesderricks@gmail.com>
2023-12-18 10:37:54 +0200

    misc: Commit for submission of the exam code repo

---
+++ src/test/java/com/derrick/demo/controller/MathControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.derrick.demo.controller;
+
+import org.junit.jupiter.api.Test;
+im
+
+import com.derrick.demo.model.DoMathRequest;
+import com.derrick.demo.model.MathResponse;
+import com.derrick.demo.service.MathOperator;
+
+
+public class MathControllerIntegrationTest {
+
+}
